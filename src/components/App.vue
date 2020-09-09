53b0f98f8ef69e0793299abe48eda66e798fc298
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-09-09 14:56:04 +0200

    first commit

---
+++ src/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>App Component displays {{ value }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            value: 'Hello World',
+        };
+    },
+};
+</script>
+
+<style></style>
